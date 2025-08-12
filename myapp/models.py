d9f924122defa9e6eda273a30dd83808ae0cc678
saqib <saqibansari2011@gmail.com>
2025-08-12 12:33:41 +0530

    expense tracker

---
+++ myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Expense(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.IntegerField()
+    cetegory = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
